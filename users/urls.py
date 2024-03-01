8a9ea12981a6ac8f9ddd12f64937f55d6313d92b
hampet1 <46238335+hampet1@users.noreply.github.com>
2024-03-01 18:14:37 +1100

    creating UI

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
 
-    path('',views.index, name="home"),
+    path('home',views.index, name="home"),
     path('guideline/', views.guideline, name='guideline'),  # Guideline page
     path('about/', views.about, name='about'),  # About project page
     path('maps/', views.maps, name='maps'),  # Maps page
