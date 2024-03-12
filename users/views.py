d7f5dc0c32b4124b396d6379a7a00c408b5b3a30
hampet1 <46238335+hampet1@users.noreply.github.com>
2024-03-12 17:33:38 +1100

    csrf token implemented

--- users/views.py
+++ users/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .forms import ElementForm
+import numpy as np
+from joblib import load
 # Create your views here.
+
+lda_model = load('lda_model_xrf.joblib')
+
+
 
 def index(request):
     # Logic for the home view
@@ -34,8 +40,39 @@ def process_elements_form(request):
         form = ElementForm(request.POST)
         if form.is_valid():
             # all values from the file
-            total_sum = sum(value for value in form.cleaned_data.values() if value is not None)
-            return JsonResponse({'total_sum': total_sum})
+            print("values: ", form.cleaned_data.values())
+            values_ = [float(value) for value in form.cleaned_data.values() if value is not None]
+            print("values now: ", values_)
+            # Ensure the input is in the correct shape (1 row with 7 columns)
+            input_array = np.array([values_])
+
+
+            if hasattr(lda_model, "predict_proba"):
+                # Get the probability estimates for all classes
+                probabilities = lda_model.predict_proba(input_array)
+
+                # Get the index of the highest probability
+                class_index = np.argmax(probabilities, axis=1)[0]
+
+                # Adjust the index to match your class labels
+                predicted_class_label = class_index + 1
+
+                # Obtain the highest probability
+                highest_probability = probabilities[0, class_index]
+
+                regions_name = {1: "Northern Calcareous Alps (Austria)",
+                                2: "St. Veit Klippen Belt (Austria)",
+                                3: "Gerecse Mountains (Hungary)",
+                                4: "Palava Hills (Czech Republic)",
+                                5: "Western Slovakia",
+                                6: "Pieniny (Poland)",
+                                7: "Artefacts (Czech Republic)"}
+
+
+                res = f"{regions_name[predicted_class_label]} with a probability of {highest_probability*100:.2f}%"
+
+                #total_sum = sum(value for value in form.cleaned_data.values() if value is not None)
+                return JsonResponse({'total_sum': res})
         else:
             # Return form errors as JSON
             return JsonResponse({'error': form.errors.as_json()}, status=400)
