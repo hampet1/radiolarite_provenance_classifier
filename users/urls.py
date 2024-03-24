603112840ee24996b25ffc564c619e11f41e8399
hampet1 <46238335+hampet1@users.noreply.github.com>
2024-03-25 10:49:21 +1100

    setting up ablation

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('home/',views.index, name="home"),
     path('guideline/', views.guideline, name='guideline'),  # Guideline page
     path('about/', views.about, name='about'),  # About project page
-    path('maps/', views.maps, name='maps'),  # Maps page
     path('classify/', views.classify, name='classify'),  # Maps page
     path('process-elements-form/', views.process_elements_form, name='process_elements_form'),
     path('test-ajax/', views.test_ajax, name='test_ajax'),
--- users/views.py
+++ users/views.py
@@ -4,7 +4,10 @@ import numpy as np
 from joblib import load
 # Create your views here.
 
-lda_model = load('lda_model_xrf.joblib')
+# for xrf using lda
+xrf_model = load('lda_model_xrf.joblib')
+# for ablation, using randomforest
+ablation_model = load('ablation_model.joblib')
 
 
 
@@ -20,10 +23,6 @@ def about(request):
     # Logic for the about project view
     return render(request, 'about.html')  # Make sure 'about_project.html' exists in your templates directory
 
-def maps(request):
-    # Logic for the maps view
-    return render(request, 'maps.html')  # Make sure 'maps.html' exists in your templates directory
-
 
 def classify(request):
     return render(request)
@@ -37,42 +36,60 @@ def process_elements_form(request):
     process all data and feed them the model
     """
     if request.method == 'POST':
+        # passing a type of the model
+        model_type = request.POST.get('method')
+        print("method is : ", model_type)
         form = ElementForm(request.POST)
         if form.is_valid():
             # all values from the file
-            print("values: ", form.cleaned_data.values())
-            values_ = [float(value) for value in form.cleaned_data.values() if value is not None]
-            print("values now: ", values_)
-            # Ensure the input is in the correct shape (1 row with 7 columns)
-            input_array = np.array([values_])
+            try:
+                print("values: ", form.cleaned_data.values())
+                values_ = [float(value) for value in form.cleaned_data.values() if value is not None]
+                print("values now: ", values_)
+                # Ensure the input is in the correct shape (1 row with 7 columns)
+                input_array = np.array([values_])
+
+                if model_type == "openXRF":
+                    model = xrf_model
+                elif model_type == "openAblation":
+                    model = ablation_model
+                elif model_type == "openMultimodal":
+                    pass
+                else:
+                    print("none of the models were used")
 
 
-            if hasattr(lda_model, "predict_proba"):
-                # Get the probability estimates for all classes
-                probabilities = lda_model.predict_proba(input_array)
 
-                # Get the index of the highest probability
-                class_index = np.argmax(probabilities, axis=1)[0]
+                if hasattr(model, "predict_proba"):
+                    # Get the probability estimates for all classes
+                    probabilities = model.predict_proba(input_array)
 
-                # Adjust the index to match your class labels
-                predicted_class_label = class_index + 1
+                    # Get the index of the highest probability
+                    class_index = np.argmax(probabilities, axis=1)[0]
 
-                # Obtain the highest probability
-                highest_probability = probabilities[0, class_index]
+                    # Adjust the index to match your class labels
+                    predicted_class_label = class_index + 1
 
-                regions_name = {1: "Northern Calcareous Alps (Austria)",
-                                2: "St. Veit Klippen Belt (Austria)",
-                                3: "Gerecse Mountains (Hungary)",
-                                4: "Palava Hills (Czech Republic)",
-                                5: "Western Slovakia",
-                                6: "Pieniny (Poland)",
-                                7: "Artefacts (Czech Republic)"}
+                    # Obtain the highest probability
+                    highest_probability = probabilities[0, class_index]
+
+                    regions_name = {1: "Northern Calcareous Alps (Austria)",
+                                    2: "St. Veit Klippen Belt (Austria)",
+                                    3: "Gerecse Mountains (Hungary)",
+                                    4: "Palava Hills (Czech Republic)",
+                                    5: "Western Slovakia",
+                                    6: "Pieniny (Poland)",
+                                    7: "Bakony Mts. (Hungary)"}
 
 
-                res = f"{regions_name[predicted_class_label]} with a probability of {highest_probability*100:.2f}%"
+                    res = f"{regions_name[predicted_class_label]} with a probability of {highest_probability*100:.2f}%"
 
-                #total_sum = sum(value for value in form.cleaned_data.values() if value is not None)
-                return JsonResponse({'total_sum': res})
+                    #total_sum = sum(value for value in form.cleaned_data.values() if value is not None)
+                    return JsonResponse({'total_sum': res})
+
+            except Exception as e:
+                print("the error is: {e}")
+
         else:
             # Return form errors as JSON
             return JsonResponse({'error': form.errors.as_json()}, status=400)
